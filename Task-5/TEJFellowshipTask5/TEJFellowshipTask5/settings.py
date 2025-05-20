a6396eb7c24d503339060a3510b7549b292d78a5
codeXpawan <shahpawan56@gmail.com>
2025-05-20 23:16:10 +0545

    Fix:Ready for production

--- Task-5/TEJFellowshipTask5/TEJFellowshipTask5/settings.py
+++ Task-5/TEJFellowshipTask5/TEJFellowshipTask5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=0#jhkdof-3$h3_9n&l(n=zot(8c1+mty9k88lqn$80i5s_2*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
